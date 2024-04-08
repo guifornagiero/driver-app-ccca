3ffa413bd9e33593e35bf736b66945f7cd7488f8
guifornagiero <guifornagiero.c@hotmail.com>
2024-04-08 17:03:03 -0300

    feat: create validation to each field

--- src/api/services/account-service.ts
+++ src/api/services/account-service.ts
@@ -1,18 +1,25 @@
-import { SignupInputDTO, SignupOutputDTO } from "../models/dto/account-dto";
-import { validateEmail, validateFields } from "../../utils/validate-fields";
+import { SignupInputDTO } from "../models/dto/account-dto";
+import { fixPlate, invalidDriver, invalidPassenger, validateEmail, validateName, validateUserType } from "../../utils/validate-fields";
 import { AccountDAL } from "../dal/account-dal";
-import { Account } from "../models/entity/account";
+import { validateCPF } from "../../utils/validate-cpf";
 
-type Output = object | SignupOutputDTO;
-
-const signup = async (account: SignupInputDTO): Promise<Output> => {
-    if(!validateFields(account)) return { message: 'Invalid fields.' };
+const signup = async (account: SignupInputDTO) => {
+    if (!validateName(account.name)) throw new Error('Invalid name.');
+    if (!validateEmail(account.email)) throw new Error('Invalid email');
+    if (!validateCPF(account.cpf)) throw new Error('Invalid CPF');
+    if (!validateUserType(account)) throw new Error('User cannot be driver and passenger.');
+    if (invalidDriver(account)) throw new Error('Invalid car plate for driver.');
+    if (invalidPassenger(account)) throw new Error('A passenger does not have a car plate.');
+    
+    fixPlate(account);
+    
+    if (!!await AccountDAL.getByEmail(account.email)) throw new Error('Email already exists on our database.');
     const id = await AccountDAL.create(account);
     return id;
 };
 
 const getAccountByEmail = async (email: string) => {
-    if(!validateEmail(email)) return { message: 'Invalid email.' };
+    if (!validateEmail(email)) throw new Error('Invalid email format.');
     const account = await AccountDAL.getByEmail(email);
     return account;
 }
