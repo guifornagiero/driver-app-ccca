ea4428fbcfd612fd4063b484ba99937f048d24c0
guifornagiero <guifornagiero.c@hotmail.com>
2024-04-15 16:52:29 -0300

    test: add coments explaning test patterns

--- tests/account-signup-service.test.ts
+++ tests/account-signup-service.test.ts
@@ -1,6 +1,7 @@
 import { AccountService } from "../src/api/services/account-service";
 import sinon from 'sinon';
 import crypto from 'crypto';
+import { AccountDAL } from "../src/api/dal/account-dal";
 
 describe('Testes para a SERVICE de Signup', () => {
     test('Deve criar uma conta para o passageiro e buscar por ID', async () => {
@@ -43,8 +44,9 @@ describe('Testes para a SERVICE de Signup', () => {
             cpf: '87748248800',
             isPassenger: true
         };
-        sinon.stub(AccountService, 'signup').resolves({ accountId: crypto.randomUUID() });
-        sinon.stub(AccountService, 'getAccount').resolves(input);
+        // Stub sobrescreve o retorno das funções signup e getAccount para os retornos que eu pré defini
+        const signupStub = sinon.stub(AccountService, 'signup').resolves({ accountId: crypto.randomUUID() });
+        const getAccountStub = sinon.stub(AccountService, 'getAccount').resolves(input);
 
         const responseSignup = await AccountService.signup(input);
         expect(responseSignup.accountId).toBeDefined();
@@ -53,5 +55,53 @@ describe('Testes para a SERVICE de Signup', () => {
         expect(responseGetAccount.name).toBe(input.name);
         expect(responseGetAccount.email).toBe(input.email);
         expect(responseGetAccount.cpf).toBe(input.cpf);
+        // Stub restaura o comportamento original das funções
+        signupStub.restore();
+        getAccountStub.restore();
+      })
+
+      test('Deve criar uma conta para o passageiro e buscar por ID com SPY', async () => {
+        const input: any = {
+            name: 'John Doe',
+            email: `john.doe${Math.round(Math.random() * 100)}@gmail.com`,
+            cpf: '87748248800',
+            isPassenger: true
+        };
+        // Spy se prepara para ficar de olho no comportamento da função getAccount
+        const getAccountSpy = sinon.spy(AccountService, "getAccount");
+        
+        const responseSignup = await AccountService.signup(input);
+        expect(responseSignup.accountId).toBeDefined();
+
+        const responseGetAccount = await AccountService.getAccount(responseSignup.accountId);
+        expect(responseGetAccount.name).toBe(input.name);
+        expect(responseGetAccount.email).toBe(input.email);
+        expect(responseGetAccount.cpf).toBe(input.cpf);
+        // Spy verifica se foi chamado uma vez, e se foi chamado com o parametro esperado
+        expect(getAccountSpy.calledOnce).toBe(true);
+        expect(getAccountSpy.calledWith(responseSignup.accountId)).toBe(true);
+      })
+
+      test.only('Deve criar uma conta para o passageiro e buscar por ID com MOCK', async () => {
+        const input: any = {
+            name: 'John Doe',
+            email: `john.doe${Math.round(Math.random() * 100)}@gmail.com`,
+            cpf: '87748248800',
+            isPassenger: true
+        };
+        // Eu digo para o Mock o que eu espero que aconteça dentro da função
+        const getAccountMock = sinon.mock(AccountDAL);
+        getAccountMock.expects("getByEmail").withArgs(input.email).once();
+
+        const responseSignup = await AccountService.signup(input);
+        expect(responseSignup.accountId).toBeDefined();
+
+        const responseGetAccount = await AccountService.getAccount(responseSignup.accountId);
+        
+        expect(responseGetAccount.name).toBe(input.name);
+        expect(responseGetAccount.email).toBe(input.email);
+        expect(responseGetAccount.cpf).toBe(input.cpf);
+        // Mock verifica se o que eu esperava que acontecesse aconteceu dentro da fn
+        getAccountMock.verify();
       })
 });
